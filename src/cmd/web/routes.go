8e4485b893020ff46fd993d9fe1af70dc27ab2b6
meru_zh <meruertzhunusova1711@gmail.com>
2023-02-26 19:54:12 +0600

    security

--- src/cmd/web/routes.go
+++ src/cmd/web/routes.go
@@ -2,12 +2,15 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middlewar
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return secureHeaders(mux)
 }
