0fe6c1680a19f6fb574a5dffa7400111853f268a
meru_zh <meruertzhunusova1711@gmail.com>
2023-05-07 16:23:57 +0600

    implement

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"moduls/models"
+	"moduls/pkg/models"
 	"net/http"
+	"os/user"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -67,10 +70,58 @@ func SignUp() gin.HandlerFunc {
 		user.Updated_At = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_ID = user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.Firts_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(htpp.StatusInternalServerError, gin.H{"error": "the user did not get created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusCreated, "Successfully signed in!")
 	}
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJson(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+		defer cancel()
+
+		if !PasswordIsValid {
+			c.JSON{http.StatusInternalServerError, gin.H{"error": msg}}
+			fmt.Println(msg)
+			return
+		}
+		token, refreshToken, _ := generate.TokenGenerator(*founderuser.Email, *founduser.Firts_Name, *founduser.Last_Name, founduser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshToken, founderuser.User_ID)
+
+		c.JSON(http.StatusFound, founduser)
+
+	}
 
 }
 
