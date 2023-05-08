8507feca5ae9fbc342fa4759b9d375661e7c36a6
meru_zh <meruertzhunusova1711@gmail.com>
2023-05-08 14:02:51 +0600

    implement 2

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	token "moduls/tokens"
+	"net/htpp"
+	"net/http"
+
+	token "github.com/akhil/ecommerce-yt/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(htpp.StatusInternalServerError, gin.H{"error": "No authorization header provided"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
